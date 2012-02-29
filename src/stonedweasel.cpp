d85216a727337959263ba3edfe05cff25672601a
Sebastian Woetzel <wose@zuendmasse.de>
2012-02-29 22:45:34 +0100

    handle keys and mousebuttons

--- src/stonedweasel.cpp
+++ src/stonedweasel.cpp
@@ -28,8 +28,12 @@ int CStonedWeasel::OnExecute() {
 
 ////////////////////////////////////////////////////////////////////////////////
 bool CStonedWeasel::OnInit() {
-    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
         return false;
+    }
+
+    SDL_WM_SetCaption("Stoned Weasel", 0);
 
     SDL_GL_SetAttribute(SDL_GL_RED_SIZE,        8);
     SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,      8);
@@ -71,8 +75,31 @@ bool CStonedWeasel::OnInit() {
 
 ////////////////////////////////////////////////////////////////////////////////
 void CStonedWeasel::OnEvent(SDL_Event* event) {
-    if(event->type == SDL_QUIT)
-        m_bRunning = false;
+    switch(event->type) {
+        case SDL_KEYDOWN:
+            switch(event->key.keysym.sym) {
+                case SDLK_ESCAPE:
+                    m_bRunning = false;
+                    break;
+                default:
+                    break;
+            }
+
+            break;
+
+        case SDL_KEYUP:
+            break;
+
+        case SDL_MOUSEBUTTONDOWN:
+            break;
+
+        case SDL_MOUSEBUTTONUP:
+            break;
+
+        case SDL_QUIT:
+            m_bRunning = false;
+            break;
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
